e0d7aea99ffd1d88ffbbbe3184fcbc33e2c5a3af
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2021-11-17 19:16:19 +0000

    Add: base files for dependency management and model script

---
+++ data_augment.py
@@ -0,0 +1,21 @@
+import os 
+import numpy as np
+import cv2
+from glob import glob
+from tqdm import tqdm
+import imageio
+from albumentations import HorizontalFlip, VerticalFlip, Rotate
+
+def load_data(path):
+    train_x = sorted(glob(os.path.join(path, "training", "images", "*.JPG")))
+    train_y = sorted(glob(os.path.join(path, "training", "1st_manual", "*.gif")))
+
+    test_x = sorted(glob(os.path.join(path, "test", "images", "*.JPG")))
+    test_y = sorted(glob(os.path.join(path, "test", "1st_manual", "*.gif")))
+
+
+if __name__== "__main__":
+    np.random.seed(42)
+
+    """ Load Data """
+    load_data('./feet_data')
