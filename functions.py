6c540f8043e584ad2b720e84723373824b9fe364
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2022-03-01 01:02:58 +0000

    Add: function to seed the randomness and calculate elapsed time

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 import os
+import random
+import numpy as np
+import torch
+
 
 """ Create dir if not exists """
 def make_dir(path):
@@ -6,3 +10,22 @@ def make_dir(path):
         print("The Specified dir already exists")
     else:
         os.mkdir(path)
+
+
+""" Seeding the randomness """
+def seeding(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+""" calculate the time taken """
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+
+    return elapsed_mins, elapsed_secs
