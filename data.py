dc2280b0251deee52ad298969d4689a7f394fff8
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2022-03-01 12:56:27 +0000

    Add: script to deal with each image and mask

---
+++ data.py
@@ -0,0 +1,35 @@
+from torch.utils.data import Dataset
+import cv2
+import numpy as np
+import torch
+
+
+class DriveDataset(Dataset):
+    def __init__(self, images_path, masks_path):
+
+        self.images_path = images_path
+        self.masks_path = masks_path
+        self.samples = len(images_path)
+
+
+    def __getitem__(self, index):
+
+        """ Reading the images """
+        image = cv2.imread(self.images_path[index], cv2.IMREAD_COLOR)
+        image = image/255.0
+        image = np.transpose(image, (2, 0, 1))
+        image = image.astype(np.float32)
+        image = torch.from_numpy(image)
+
+        """ Reading the masks """
+        mask = cv2.imread(self.masks_path[index], cv2.IMREAD_COLOR)
+        mask = mask/255.0
+        mask = np.expand_dims(mask, axis=0)
+        mask = mask.astype(np.float32)
+        mask = torch.from_numpy(mask)
+
+        return image, mask
+
+    def __len__(self):
+        return self.samples
+
