3023328dae033f259c50bfbd013d2ce4aa107b75
Rui Mesquita <ruipedrorochamesquita@gmail.com>
2022-03-01 19:50:45 +0000

    Add: code to train the model

--- train.py
+++ train.py
@@ -1,7 +1,54 @@
 from glob import glob
-from os import mkdir
-from functions import seeding, make_dir, epoch_time
+import os
+from random import shuffle
+from sched import scheduler
+from tabnanny import verbose
+import time
 
+from functions import seeding, make_dir, epoch_time
+from loss import DiceLoss, DiceBCELoss
+from model import build_unet
+from data import DriveDataset
+
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+
+
+def train(model, loader, optimizer, loss_fn, device):
+    epoch_loss = 0.0
+
+    model.train()
+    for x, y in loader:
+        x = x.to(device, dtype=torch.float32)
+        y = y.to(device, dtype=torch.float32)
+
+        optimizer.zero_grad()
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    epoch_loss = epoch_loss/len(loader)
+    return epoch_loss
+
+
+def evaluate(model, loader, loss_fn, device):
+    epoch_loss = 0.0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device, dtype=torch.float32)
+            y = y.to(device, dtype=torch.float32)
+
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            epoch_loss += loss.item()
+
+        epoch_loss = epoch_loss/len(loader)
+    return epoch_loss
 
 if __name__ == "__main__":
     """ Seeding """
@@ -19,3 +66,63 @@ if __name__ == "__main__":
 
     meta_data = f"Dataset Size:\nTrain: {len(train_x)} - Valid: {len(valid_x)}"
     print(meta_data)
+
+    """ Training parameter """
+    H = 512
+    W = 512
+    size = (H, W)
+    batch_size = 2
+    num_epochs = 50
+    lr = 1e-4
+    checkpoint_path = "files/checkpoint.pth"
+
+    """ Dataset and loader """
+    train_dataset = DriveDataset(train_x, train_y)
+    valid_dataset = DriveDataset(valid_x, valid_y)
+
+    train_loader = DataLoader (
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=2
+    )
+
+    valid_loader = DataLoader (
+        dataset=valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=2
+    )
+
+    device = torch.device("cuda") ## GTX 1660 Ti
+    model = build_unet()
+    model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_schedular.ReduceLROnPlateau(optimizer, 'min', patience=5, verbose=True)
+    loss_fn = DiceBCELoss()
+
+    """ Training the model """
+    best_valid_loss = float('inf')
+
+    for epoch in range(num_epochs):
+        start_time = time.time()
+
+        train_loss = train(model, train_loader, optimizer, loss_fn, device)
+        valid_loss = evaluate(model, valid_loader, loss_fn, device)
+
+        """ Saving the model """
+        if valid_loss < best_valid_loss:
+            data_str = f'Valid loss improved from {best_valid_loss:2.4f} to {valid_loss:2.4f}. Saving checkpoint: {checkpoint_path}'
+            print(data_str)
+
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), checkpoint_path)
+        
+        end_time = time.time()
+        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+        data_str = f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s\n'
+        data_str += f'\tTrain Loss: {train_loss:.3f}\n'
+        data_str += f'\t Val. Loss: {valid_loss:.3f}\n'
+        print(data_str)   
