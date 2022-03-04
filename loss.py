Fix: intersection calculation

loss.py
@@ -31,8 +31,7 @@ class DiceBCELoss(nn.Module):
         targets = targets.view(-1)
 
         intersection = (inputs * targets).sum()
-        dice_loss = 1 - (2.*intersection + smooth) / \
-            (inputs.sum() + targets.sum() + smooth)
+        dice_loss = 1 - (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
         BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
         dice_BCE = BCE + dice_loss
 